7619acdfaa3fc96dcada284e432403191b524a9b
Edward Smith <esmith@branch.io>
2018-05-14 11:09:24 -0700

    Layout project.

---
+++ Branch/BranchFramework.h
@@ -0,0 +1,19 @@
+//
+//  Branch_SDK_Mac.h
+//  Branch-SDK-Mac
+//
+//  Created by Edward on 5/11/18.
+//  Copyright © 2018 Branch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Branch_SDK_Mac.
+FOUNDATION_EXPORT double Branch_SDK_MacVersionNumber;
+
+//! Project version string for Branch_SDK_Mac.
+FOUNDATION_EXPORT const unsigned char Branch_SDK_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Branch_SDK_Mac/PublicHeader.h>
+
+
