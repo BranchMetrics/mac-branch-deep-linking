00820af7f8678ecb750376a425843ec7d69e08b7
nsingh-branch <nipun.singh@branch.io>
2023-08-22 10:49:43 -0700

    Removed old identity code

--- Branch/BNCNetworkAPIService.m
+++ Branch/BNCNetworkAPIService.m
@@ -417,8 +417,6 @@ static NSString*_Nonnull BNCNetworkQueueFilename =  @"io.branch.sdk.network_queu
 
     if (self.session.linkCreationURL.length)
         self.settings.linkCreationURL = self.session.linkCreationURL;
-    if (self.session.userIdentityForDeveloper.length)
-        self.settings.userIdentityForDeveloper = self.session.userIdentityForDeveloper;
     if (self.session.sessionID.length)
         self.settings.sessionID = self.session.sessionID;
        
